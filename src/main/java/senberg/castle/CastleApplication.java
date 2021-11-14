c7303717899a602483a8dedd06bbb005958bc101
Staffan Enberg <senberg@live.se>
2021-11-15 00:01:16 +0100

    Initial commit

---
+++ src/main/java/senberg/castle/CastleApplication.java
@@ -0,0 +1,13 @@
+package senberg.castle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senberg/castle/CastleApplicationTests.java
@@ -0,0 +1,13 @@
+package senberg.castle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
