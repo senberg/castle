0b305e2b3aa7d3fd0f80dac379b1959b8542fa07
Staffan Enberg <senberg@live.se>
2021-11-16 00:01:16 +0100

    Island

---
+++ src/main/java/senberg/island/Island.java
@@ -0,0 +1,13 @@
+package senberg.island;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Island {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Island.class, args);
+	}
+
+}
---
+++ src/test/java/senberg/island/IslandTests.java
@@ -0,0 +1,13 @@
+package senberg.island;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslandTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
